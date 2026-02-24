70a3f9d7e0477b47a92a11b5837acf3546308767
KANEDA12345 <git@stackblitz.com>
2026-02-24 13:15:08 +0900

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,41 @@
+import './style.css';
+
+const onClickAdd = () => {
+  const inputText = document.getElementById('add-text').value;
+  document.getElementById('add-text').value = '';
+
+  // li生成
+  const li = document.createElement('li');
+  // div生成
+  const div = document.createElement('div');
+  div.className = 'list-row';
+  // pタグ生成
+  const p = document.createElement('p');
+  p.className = 'todo-item';
+  p.innerText = inputText;
+
+  // button(完了）タグ生成
+  const completeButton = document.createElement('button');
+  completeButton.innerText = '完了';
+  completeButton.addEventListener('click', () => {
+    alert();
+  });
+  // button(削除）タグ生成
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = '削除';
+  deleteButton.addEventListener('click', () => {
+    alert();
+  });
+  //console.log(deleteButton);
+
+  // liタグの子要素に各要素を設定
+  div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  li.appendChild(div);
+
+  // 未完了リストに追加
+  document.getElementById('incomplete-list').appendChild(li);
+};
+
+document.getElementById('add-button').addEventListener('click', onClickAdd);
